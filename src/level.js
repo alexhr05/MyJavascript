189997714bf3a4609af4502a077a78302362bcd6
Alex <alexhr05@gmail.com>
2020-01-13 15:08:58 +0200

    level

---
+++ src/level.js
@@ -0,0 +1,41 @@
+
+const level = function () {
+    function trasnformMap(_map) {
+        return _map.map(x => x.split(""));
+    }
+    
+    this.map = trasnformMap([
+        "*******************",
+        "*                 *",
+        "*         *       *",
+        "*    D    *        ",
+        "*         *        ",
+        "*         *        ",
+        "*                  ",
+        "*      D           ",
+        "*                  ",
+        "*                  ",
+        "***               *",
+        "*          D      *",
+        "*              ****",
+        "*******************"
+    ]);
+    this.x = 1;
+    this.y = 1;
+    this.vX = 3;
+    this.vY = 3;
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+}();
