226b67f87c2492e6930c29130b1bdcd14704b7e8
Beti66 <57043534+Beti66@users.noreply.github.com>
2020-04-09 21:45:16 +0300

    Some_problems_solved

--- web/src/event.js
+++ web/src/event.js
@@ -1,29 +1,62 @@
 const event = new function () {
 
+	let keylog = [];
 
 	this.init = function () {
 		console.log("neshto");
-		document.addEventListener('keydown', KeyPressed);
+		document.addEventListener('keydown', KeyDown);
+		document.addEventListener('keyup', KeyUp);
 		console.log("drugo");
+
 	}
-	function KeyPressed(keyCode) {
-		console.log(keyCode.code);
+	function KeyUp(eventKeyUp) {
+		console.log("....key+UP++" + eventKeyUp.code);
+
+		for (var i = 0; i < keylog.length; i++) {
+			if (keylog[i] == eventKeyUp.code)
+				keylog.splice(i);
+		}
+
+		for (var i = 0; i < keylog.length; i++) {
+			console.log("=NE natisnat, ot masiv ===i=" + i + "; keylog[i]=" + keylog[i]);
+		}
+		console.log(keylog.length);
+
+	}
+	function KeyDown(eventKeyDown) {
+		console.log("++++++++++++++" + eventKeyDown.code);
 		this.gravityY = 1;		// Гравитацията се задейства при всеки бутон
-		
-		if (keyCode.code == "ArrowDown") {		// Стрелка надолу
+
+		if (keylog.indexOf(eventKeyDown.code) == -1)
+			keylog.push(eventKeyDown.code);
+
+
+		for (var i = 0; i < keylog.length; i++) {
+			console.log("=DOWN, ot masiv ===i=" + i + "; keylog[i]=" + keylog[i]);
+		}
+
+	}
+
+	function handleKey(keyCode) {
+		if (keyCode == "ArrowDown") {		// Стрелка надолу
 			game.handleMove(0, 1);
 		}
-		if (keyCode.code == "ArrowUp") {
+		if (keyCode == "ArrowUp") {
 			game.handleMove(0, -1);
 		}
-		if (keyCode.code == "ArrowLeft") {
+		if (keyCode == "ArrowLeft") {
 			game.handleMove(-1, 0);
 		}
-		if (keyCode.code == "ArrowRight") {
+		if (keyCode == "ArrowRight") {
 			game.handleMove(1, 0);
 		}
 	}
+	function handleKeys(keyCode) {
+		console.log("Vliza v HandleKeys");
 
-
+		for (var i = 0; i < keylog.length; i++) {
+			handleKey(keylog[i]);
+		}
+	}
 
 }();
