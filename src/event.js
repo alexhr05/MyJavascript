584a1ab0ceb13f2996bd94a4337ca82e100b9bab
Beti66 <57043534+Beti66@users.noreply.github.com>
2020-04-24 23:35:41 +0300

    Incorrect use of vY

---
+++ src/event.js
@@ -0,0 +1,48 @@
+const event = new function () {
+
+	let keylog = [];
+
+	this.init = function () {
+		document.addEventListener('keydown', KeyDown);
+		document.addEventListener('keyup', KeyUp);
+		setInterval(handleKeys, 20);
+	}
+	function KeyUp(eventKeyUp) {
+
+
+		for (var i = 0; i < keylog.length; i++) {
+			if (keylog[i] == eventKeyUp.code)
+				keylog.splice(i);
+		}
+	}
+	function KeyDown(eventKeyDown) {
+		if (keylog.indexOf(eventKeyDown.code) == -1)
+			keylog.push(eventKeyDown.code);
+	}
+
+	function handleKey(keyCode) {
+		if (keyCode == "ArrowDown") {		// Стрелка надолу
+			game.handleMove(0, 1);
+		}
+		if (keyCode == "ArrowUp") {
+			level.vY -= 1;
+		}
+		if (keyCode == "ArrowLeft") {
+			game.handleMove(-1, 0);
+		}
+		if (keyCode == "ArrowRight") {
+			game.handleMove(1, 0);
+		}
+	}
+	function handleKeys() {
+
+		for (var i = 0; i < keylog.length; i++) {
+			handleKey(keylog[i]);
+		}
+	}
+	
+
+
+	
+
+}();
