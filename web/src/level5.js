f8097a8fe7659b1bb15e0987e93008866958f89e
Betina <beti105@abv.bg>
2020-06-15 00:34:57 +0300

    New level

---
+++ web/src/level5.js
@@ -0,0 +1,29 @@
+const level5 = new function (){
+    
+    this.map = () => [
+        "                       cc",
+        "                       cc",
+        "                       cc",
+        "                       cc",
+        "                       cc",
+        "                     D cc",
+        "                  D	cc",
+        "D              D     q cc",
+        "   D       D      q    cc",
+        "q     D        q       cc",
+        "   q     q  q          cc",
+        "      q                cc",
+        "                       cc",
+        "wwwwwwwwwwwwwwwwwwwwwwwww",
+        "wwwwwwwwwwwwwwwwwwwwwwwww",
+        "wwwwwwwwwwwwwwwwwwwwwwwww"
+    ];
+
+    this.x = () => 15;
+    this.y = () => 15;
+    this.vX = () => 3;
+    this.vY = () => 3;
+    this.squareSizeX = () => 10;
+    this.squareSizeY = () => 10;
+    
+}();
