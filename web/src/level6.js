4d6310832e8307579b0758b0041070d10027840f
Betina <beti105@abv.bg>
2020-06-15 01:32:40 +0300

    New level

---
+++ web/src/level6.js
@@ -0,0 +1,29 @@
+const level6 = new function (){
+    
+    this.map = () => [
+        "                       cc",
+        "   o   o     o       o cc",
+        "                       cc",
+        "  o             o      cc",
+        "        o              cc",
+        "                       cc",
+        "                   	cc",
+        "              D     D  cc",
+        "                       cc",
+        "           D  *     *  cc",
+        "                 *     cc",
+        "           *           cc",
+        "       D               cc",
+        "ggggggggggggggggggggggggg",
+        "ggggggggggggggggggggggggg",
+        "ggggggggggggggggggggggggg"
+    ];
+
+    this.x = () => 15;
+    this.y = () => 15;
+    this.vX = () => 3;
+    this.vY = () => 3;
+    this.squareSizeX = () => 10;
+    this.squareSizeY = () => 10;
+    
+}();
