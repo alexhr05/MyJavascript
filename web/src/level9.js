c813cf999234cf968340cc16c8ded4cffbbed037
Betina <beti105@abv.bg>
2020-06-15 17:58:50 +0300

    New level

---
+++ web/src/level9.js
@@ -0,0 +1,29 @@
+const level9 = new function (){
+    
+    this.map = () => [
+        "                       cc",
+        "                       cc",
+        "                       cc",
+        "                       cc",
+        "                       cc",
+        "                       cc",
+        "                     	cc",
+        "                       cc",
+        " D                     cc",
+        "     D               D cc",
+        " *       D      D      cc",
+        "     *      D        * cc",
+        "         *      *      cc",
+        "            *          cc",
+        "wwwwwwwwwwwwwwwwwwwwwwwww",
+        "wwwwwwwwwwwwwwwwwwwwwwwww"
+    ];
+
+    this.x = () => 15;
+    this.y = () => 15;
+    this.vX = () => 3;
+    this.vY = () => 3;
+    this.squareSizeX = () => 10;
+    this.squareSizeY = () => 10;
+    
+}();
